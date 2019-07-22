92f1d2d338155a5e759fae84d0680432ce71f0d8
athreya <athreya.joshi10@gmail.com>
2019-07-21 21:49:18 -0400

    changed the code in dev branch

--- src/test/java/myCompany/selenium.java
+++ src/test/java/myCompany/selenium.java
@@ -10,6 +10,7 @@ public class selenium {
 	public void BrowserAutomation() {
 		System.out.println("Modified code in MavenDemoGitDemo");
 		System.out.println("Added 1 more line by 2nd person for practice purpose");
+		System.out.println("switched to dev branch and changing program");
 	}
 	
 	@Test
