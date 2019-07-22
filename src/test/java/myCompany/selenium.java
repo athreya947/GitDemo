6375582eb2e9afe1b6e0da589e1bba9dfe04e8c6
athreya <athreya.joshi10@gmail.com>
2019-07-21 22:04:23 -0400

    Added in new code by athreya

--- src/test/java/myCompany/selenium.java
+++ src/test/java/myCompany/selenium.java
@@ -11,6 +11,7 @@ public class selenium {
 		System.out.println("Modified code in MavenDemoGitDemo");
 		System.out.println("Added 1 more line by 2nd person for practice purpose");
 		System.out.println("switched to dev branch and changing program");
+		System.out.println("I am updating in 07212019 package");
 	}
 	
 	@Test
